6a11e2914cf2df53c2941d70cacd2f2a76358e43
Mathis Zeiher <mathis.zeiher@gmail.com>
2024-10-27 21:11:06 +0100

    chore: added zls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,6 +38,8 @@ return {
 
 			lspconfig.yamlls.setup({})
 
+			lspconfig.zls.setup({})
+
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
